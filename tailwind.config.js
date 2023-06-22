fb1aa2033c71c4d5f93bf2158aae0178ae0596b1
Elvann <elvann.abendroth@gmail.com>
2023-06-22 03:04:16 +0300

    ⭐Init

---
+++ tailwind.config.js
@@ -0,0 +1,143 @@
+/** @type {import('tailwindcss').Config} */
+
+const themeSwapper = require('tailwindcss-theme-swapper')
+const colors = require('tailwindcss/colors')
+
+const darkest = colors.gray[950]
+const lightest = colors.gray[100]
+
+const themeSwapperPlugin = themeSwapper({
+  themes: [
+    {
+      name: 'base',
+      selectors: [':root'],
+      theme: {
+        colors: {
+          background: lightest,
+          foreground: darkest,
+          border: colors.gray[200],
+          input: colors.gray[200],
+          ring: colors.gray[200],
+          primary: {
+            DEFAULT: colors.violet[500],
+            hover: colors.violet[400],
+            foreground: lightest,
+          },
+          secondary: {
+            DEFAULT: colors.lime[500],
+            hover: colors.lime[400],
+            foreground: darkest,
+          },
+          destructive: {
+            DEFAULT: colors.red[500],
+            hover: colors.red[400],
+            foreground: lightest,
+          },
+          success: {
+            DEFAULT: colors.teal[500],
+            hover: colors.teal[400],
+            foreground: darkest,
+          },
+          warn: {
+            DEFAULT: colors.teal[500],
+            hover: colors.teal[400],
+            foreground: lightest,
+          },
+          muted: {
+            DEFAULT: colors.gray[500],
+            hover: colors.gray[400],
+            foreground: lightest,
+          },
+          accent: {
+            DEFAULT: colors.teal[600],
+            hover: colors.teal[500],
+            foreground: lightest,
+          },
+          popover: {
+            DEFAULT: lightest,
+            foreground: darkest,
+          },
+          card: {
+            DEFAULT: lightest,
+            foreground: darkest,
+          },
+        },
+      },
+    },
+    {
+      name: 'dark',
+      selectors: ['.dark', '[data-theme="dark"]'],
+      theme: {
+        colors: {
+          background: darkest,
+          foreground: lightest,
+          primary: {
+            DEFAULT: colors.violet[500],
+            hover: colors.violet[400],
+            foreground: lightest,
+          },
+          border: colors.gray[800],
+          input: colors.gray[950],
+          ring: colors.gray[800],
+          muted: {
+            DEFAULT: colors.gray[600],
+            hover: colors.gray[500],
+            foreground: lightest,
+          },
+          popover: {
+            DEFAULT: darkest,
+            foreground: lightest,
+          },
+          card: {
+            DEFAULT: darkest,
+            foreground: lightest,
+          },
+        },
+      },
+    },
+  ],
+})
+
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      borderRadius: {
+        lg: '0.6rem',
+        md: '0.3rem',
+        sm: '0.1rem',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwindcss-animate'),
+    themeSwapperPlugin,
+  ],
+}
